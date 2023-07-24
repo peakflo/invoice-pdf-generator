a4e1d7d8d738f4549b0a9a43f17a7947729c0ab9
dimavedenyapin <dimavedenyapin@users.noreply.github.com>
2023-07-24 04:18:23 +0000

    Update v0.2.0-alpha to output generated at af0e58b

---
+++ index.d.ts
@@ -0,0 +1,5 @@
+import { OutputType } from "./src/index";
+import { jsPDF } from "./src/index";
+import jsPDFInvoiceTemplate from "./src/index";
+import { jsPDFRfqTemplate } from "./src/index";
+export { OutputType, jsPDF, jsPDFInvoiceTemplate, jsPDFRfqTemplate };
