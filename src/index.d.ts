162f22f267fc8842a8ec97341942181d419e47f5
ChaudharyRaman <ramanchaudhary2704@gmail.com>
2024-02-22 17:31:15 +0530

    pdf fix: overflow and overlap text

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,15 +1,14 @@
 export default jsPDFInvoiceTemplate;
-import jsPDFRfqTemplate from "./RFQ/index";
-declare const OutputType: {
-  Save: "save"; //save pdf as a file
-  DataUriString: "datauristring"; //returns the data uri string
-  DataUri: "datauri"; //opens the data uri in current window
-  DataUrlNewWindow: "dataurlnewwindow"; //opens the data uri in new window
-  Blob: "blob"; //return blob format of the doc,
-  ArrayBuffer: "arraybuffer"; //return ArrayBuffer format
-};
-
+export namespace OutputType {
+    const Save: string;
+    const DataUriString: string;
+    const DataUri: string;
+    const DataUrlNewWindow: string;
+    const Blob: string;
+    const ArrayBuffer: string;
+}
 import { jsPDF } from "jspdf";
+import jsPDFRfqTemplate from "./RFQ";
 /**
  *
  * @param {
@@ -34,22 +33,33 @@ import { jsPDF } from "jspdf";
  *       addressLine3?: string,
  *       email_1?: string,
  *       country?: string,
+ *       taxNumber?: string
  *   },
  *   contact?: {
  *       label?: string,
  *       name?: string,
- *       address?: string,
- *       addressLine2?: string,
- *       addressLine3?: string,
- *       country?: string,
+ *       billingAddress: {
+ *          address?: string,
+ *          addressLine2?: string,
+ *          addressLine3?: string,
+ *          country?: string,
+ *       },
+ *       shippingAddress?: {
+ *          address?: string,
+ *          addressLine2?: string,
+ *          addressLine3?: string,
+ *          country?: string,
+ *       }
  *   },
  *   data?: {
  *       label?: string,
- *       num?: number,
+ *       num?: number | string,
  *       date1Label?: string
  *       date1?: string,
  *       date2Label?: string
  *       date2?: string,
+ *       netTermLabel?: string,
+ *       netTerm?: string,
  *       headerBorder?: boolean,
  *       tableBodyBorder?: boolean,
  *       header?: string[],
@@ -59,14 +69,52 @@ import { jsPDF } from "jspdf";
  *       subTotal?: string,
  *       currency?: string,
  *       descLabel?: string,
- *       customFields?: string[],
  *       desc?: string[],
  *       creditNoteLabel?: string,
  *       note?: string,
+ *       requestedBy?: string,
+ *       customFields?: string[],
+ *       authorisedBy?: string,
+ *       pdfTitle?: string,
+ *       staticVA?: {
+ *          account: string,
+ *          bank: string,
+ *       },
+ *      indiaIRP?: {
+ *          qrCode: string,
+ *          irn: string,
+ *          label4: string,
+ *          gstRegType: string,
+ *          label1: string,
+ *          hsnNum: string,
+ *          label3: string,
+ *          gstStateWithCode: string
+ *          label2: string,
+ *          label5: string,
+ *          label6: string,
+ *          ackDate: string,
+ *          ackNumber: string,
+ *      },
+ *      eSign?: {
+ *          approverName?: string,
+ *          approvedAt?: string,
+ *          authorizer?: string,
+ *          approverText?: string,
+ *          signature?: {
+ *              src?: string,
+ *              width?: number,
+ *              height?: number,
+ *              margin?: {
+ *                  top?: number,
+ *                  left?: number
+ *              }
+ *          },
+ *      },
  *       row1?: {
  *           col1?: string,
  *           col2?: string,
  *           col3?: string,
+ *           col4?: any,
  *           style?: {
  *               fontSize?: number
  *           },
@@ -80,16 +128,24 @@ import { jsPDF } from "jspdf";
  *               fontSize?: number
  *           }
  *       },
+ *       row3?: {
+ *           col1?: string,
+ *           col2?: string,
+ *           col3?: string,
+ *           style?: {
+ *               fontSize?: number
+ *           }
+ *       },
  *       total?: {
  *           col1?: string,
  *           col2?: string,
  *           col3?: string,
- *           totalConv?: string;
- *           totalTaxAmount: string;
- *           totalTaxAmountConv: string;
- *           defaultCurrency?: string;
- *           convRate?: string;
- *           isFxConversionVisible: boolean;
+ *           col4?: string,
+ *           col5?: string,
+ *           totalConv?: string,
+ *           isFxConversionVisible: boolean,
+ *           defaultCurrency?: string,
+ *           convRate?: string,
  *           style?: {
  *               fontSize?: number
  *           }
@@ -100,132 +156,7 @@ import { jsPDF } from "jspdf";
  *   pageEnable?: boolean,
  *   pageLabel?: string, } } props
  */
-declare function jsPDFInvoiceTemplate(props: {
-  outputType: string;
-  returnJsPDFDocObject?: boolean;
-  fileName: string;
-  orientationLandscape?: boolean;
-  pdfTitle?: string;
-  logo?: {
-    src?: string;
-    width?: number;
-    height?: number;
-    margin?: {
-      top?: number;
-      left?: number;
-    };
-  };
-  business?: {
-    name?: string;
-    address?: string;
-    phone?: string;
-    email?: string;
-    email_1?: string;
-    website?: string;
-  };
-  contact?: {
-    label?: string;
-    name?: string;
-    address?: string;
-    phone?: string;
-    email?: string;
-    otherInfo?: string;
-    taxNumber?: string;
-  };
-  data?: {
-    label?: string;
-    subTotalLabel?: string;
-    subTotal?: string;
-    num?: number | string;
-    date1?: string;
-    date1Label?: string;
-    date2?: string;
-    date2Label?: string;
-    headerBorder?: boolean;
-    tableBodyBorder?: boolean;
-    header?: string[];
-    headerWidth?: number[];
-    table?: any;
-    currency?: string;
-    descLabel?: string;
-    desc?: string[];
-    note?: string;
-    row1?: {
-      col1?: string;
-      col2?: string;
-      col3?: string;
-      style?: {
-        fontSize?: number;
-      };
-      hideTotal?: boolean;
-    };
-    row2?: {
-      col1?: string;
-      col2?: string;
-      col3?: string;
-      style?: {
-        fontSize?: number;
-      };
-    };
-    row3?: {
-      col1?: string;
-      col2?: string;
-      col3?: string;
-      style?: {
-        fontSize?: number;
-      };
-    };
-    total?: {
-      col1?: string;
-      col2?: string;
-      col3?: string;
-      totalTaxAmount: string;
-      totalTaxAmountConv: string;
-      subTotalConv?: string;
-      totalConv?: string;
-      defaultCurrency?: string;
-      convRate?: string;
-      isFxConversionVisible: boolean;
-      style?: {
-        fontSize?: number;
-      };
-    };
-    indiaIRP?: {
-      qrCode?: string;
-      irn?: string;
-      ackDate?: string;
-      ackNumber?: string;
-    };
-    eSign?: {
-      approverName?: string;
-      approvedAt?: string;
-      authorizer?: string;
-      approverText?: string;
-      signature?: {
-        src?: string;
-        width?: number;
-        height?: number;
-        margin?: {
-          top?: string;
-          left?: string;
-        };
-      };
-    };
-  };
-  footer?: {
-    text?: string;
-  };
-  pageEnable?: boolean;
-  pageLabel?: string;
-}): {
-  pagesNumber: number;
-  jsPDFDocObject?: jsPDF;
-  blob?: Blob;
-  dataUriString?: string;
-  arrayBuffer?: ArrayBuffer;
-  estamp?: {
-    pageNumberForInvoiceTotal?: number;
-    invoiceTotalLineHeight?: number;
-  };
-};
-export { jsPDF, OutputType, jsPDFRfqTemplate };
+declare function jsPDFInvoiceTemplate(props: any): Promise<{
+    pagesNumber: number;
+}>;
+export { jsPDF, jsPDFRfqTemplate };
